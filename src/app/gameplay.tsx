9e3886c9660d5f744af35d05b4736a68915e824e
Victoria Lu <victoria.lu88@gmail.com>
2024-07-01 16:42:17 -0400

    Create gameplay UI + add css modules

---
+++ src/app/gameplay.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import styles from './gameplay.module.css';
+
+export default function Gameplay() {
+  return (
+    <div className={styles.content}>
+      <div className={styles.pictureContainer}>
+        <img src="" className={styles.genImg} />
+      </div>
+      <div>
+        <p>placeholder for content</p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import ReactCardFlip from 'react-card-flip';
-
+import styles from './page.module.css';
 
 export default function Page() {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -18,33 +18,33 @@ export default function Page() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col bg-[#F0EAF4]">
-      <header className="flex h-20 w-full items-center justify-between bg-white px-24 md:h-24">
-        <div className="text-black text-lg font-bold">PIXLET.</div>
+    <main className={styles.homepage}>
+      <header className={styles.navBar}>
+        <div className={styles.navTitle}>PIXLET.</div>
         <nav className="flex space-x-4">
-          <a href="#about" className="text-[#7A7A7A] text-lg font-semibold px-2">About</a>
-          <a href="#contact" className="text-[#7A7A7A] text-lg font-semibold px-2">Contact</a>
-          <a href="#donate" className="text-[#7A7A7A] text-lg font-semibold px-2">Donate</a>
+          <a href="#about" className={styles.navItem}>About</a>
+          <a href="#contact" className={styles.navItem}>Contact</a>
+          <a href="#donate" className={styles.navItem}>Donate</a>
         </nav>
       </header>
-      <div className="flex grow flex-col items-center justify-center gap-4 mt-10">
+      <div className={styles.content}>
         <ReactCardFlip flipDirection="vertical" isFlipped={isFlipped}>
           <div>
-            <img src="/flip-watermelon.svg" alt="flipping flower animation" className="w-20 h-20" />
+            <img src="/flip-watermelon.svg" alt="flipping flower animation" className={styles.animation} />
           </div>
           <div>
-            <img src="/flip-flower.svg" alt="flipping watermelon animation" className="w-20 h-20" />
+            <img src="/flip-flower.svg" alt="flipping watermelon animation" className={styles.animation} />
           </div>
         </ReactCardFlip>
-        <p className="font-semibold text-2xl text-gray-800 md:text-3xl md:leading-normal text-center">
+        <p className={styles.subtitle}>
           Get 5 chances to guess why today matters
         </p>
         <div className="flex space-x-4">
           {/* tktk - bolder should be modulized */}
-          <button className="bg-transparent border border-[#32517D] text-[#32517D] font-medium px-6 py-2 rounded-full">
+          <button className={styles.primaryButton}>
             How to play
           </button>
-          <button className="bg-[#32517D] text-white font-medium px-6 py-2 rounded-full">
+          <button className={styles.secondaryButton}>
             Play
           </button>
         </div>
